286bce20c3fef4c6227e2ccb6fbd561b8f790cf2
Andrey <158809049+AndreyDimitrov3@users.noreply.github.com>
2024-09-24 21:31:01 +0300

    Update and rename Counter.js to counter.js

--- #2 Counter/counter.js
+++ #2 Counter/counter.js
@@ -1,20 +1,22 @@
+document.addEventListener('DOMContentLoaded', function(){
     let value = document.getElementById('value');
     let count = 0;
-
+    
     document.getElementById('plus').addEventListener('click', plus)
     function plus(){
         count += 1;
-        value.innerText = count
+        value.innerText = count;
     }
-
+    
     document.getElementById('minus').addEventListener('click', minus)
     function minus(){
         count -= 1;
-        value.innerText = count
+        value.innerText = count;
     }
-
+    
     document.getElementById('reset').addEventListener('click', reset)
     function reset(){
         count = 0;
-        value.innerText = count
-    }
+        value.innerText = count;
+    }  
+});
